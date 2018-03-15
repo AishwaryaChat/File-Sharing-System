b1a3edca8cc4e468aaad927795c5f2cf6595ce11
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2018-03-15 11:06:31 +0530

    Add requestmethod for files to add a file

--- common/models/files.js
+++ common/models/files.js
@@ -1,5 +1,40 @@
 'use strict';
 
+var LoopBackContext = require('loopback-context')
+
 module.exports = function(Files) {
+  Files.addOrganisation = (name, content, userId, cb) => {
+    Files.create({name, content, userId})
+    .then(file => {
+      cb(null, file)
+    })
+    .catch(err => {
+      console.log(err)
+      cb(err)
+    })
+  }
+  Files.remoteMethod(
+    'addOrganisation', {
+      http: {
+        path: '/',
+        verb: 'post'
+      },
+      accepts: [{
+        arg: 'name',
+        type: 'string'
+      }, {
+          arg: 'content',
+          type: 'string'
+      }, {
+          arg: 'userId',
+          type: 'string'
+      }
+    ],
+      returns: {
+          arg: 'data',
+          type: 'string'
+      }
+    }
+  )
 
 };
