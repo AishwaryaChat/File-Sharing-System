d43c7c0c84a4f842995e15c78f3d160e233f94b9
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2018-03-16 23:12:48 +0530

    Add api to create an organisation

--- common/models/organisation.js
+++ common/models/organisation.js
@@ -1,5 +1,38 @@
 'use strict';
 
 module.exports = function(Organisation) {
-
+  Organisation.createOrganisation = (name, options, cb) => {
+    Organisation.findOne({admin: options.accessToken.userId})
+    .then(organisation => {
+      if (organisation) return Promise.reject('error.organisation already exist')
+      return Organisation.create({name, admin: options.accessToken.userId})
+    })
+    .then(organisation => cb(null, organisation))
+    .catch(err => {
+      console.log(err)
+      cb(err)
+    })
+  }
+  Organisation.remoteMethod(
+    'createOrganisation', {
+      http: {
+        path: '/',
+        verb: 'post'
+      },
+      accepts: [
+        {
+          arg: 'name',
+          type: 'string'
+        },
+        {
+          "arg": "options",
+          "type": "object",
+          "http": "optionsFromRequest"
+        }],
+      returns: {
+        type: 'object',
+        root: true
+      }
+    }
+  )
 };
