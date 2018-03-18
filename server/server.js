5e2b72b515217b48dc8aea2789f6aaaeb2efa810
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2018-03-18 20:11:04 +0530

    Add procfile

--- server/server.js
+++ server/server.js
@@ -7,7 +7,8 @@ var app = module.exports = loopback();
 
 app.start = function() {
   // start the web server
-  return app.listen(function() {
+  const port = process.env.PORT || 3000
+  return app.listen(port, function() {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
